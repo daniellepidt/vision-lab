413014d68d622ee097b021067d662d5813e41bc7
daniellepidt <pidtylok@mail.tau.ac.il>
2022-11-01 23:48:41 +0200

    half way there, stuck on get_color() func

--- 04 - detectColor.py
+++ 04 - detectColor.py
@@ -9,15 +9,15 @@ class DetectColor():
     """	
     def __init__(self):
         cv2.namedWindow('img')
-        cv2.setMouseCallback(___,___)
+        cv2.setMouseCallback('img',self.get_color(cv2.EVENT_FLAG_LBUTTON,cv2.get,,cv2.COLOR_BGR2GRAY,param="img"))
         #Variable that enable the tracking only after the first click.
         self.firstClick=False
 
     def get_color(self,event,x,y,flags,param):
         #if left mouse was clicked
-        if event == cv2.___________:
-            self.x=__
-            self.y=___
+        if event == cv2.EVENT_FLAG_LBUTTON:
+            self.x=x
+            self.y=y
             self.firstClick=True
             #Variable that enable the computation of the color to track only once
             #after each click
@@ -25,36 +25,36 @@ class DetectColor():
 
     def main(self):
         #Enable video
-        cap = ________________(0)
-        while ___________:
+        cap = cv2.VideoCapture(0)
+        while True:
             #Read from camera
-            _, frame = ____.read()
+            self.img, frame = cap.read()
             #Show stream on 'img' window
-            cv2.___________(_____________)
+            cv2.imshow('image',self.img)
             #Wait to show stream
-            k = ____________
+            k = cv2.waitKey(0) & 0xFF
             #Escape breaks the code
-            if ___________:
-                ___________
+            if k==27:
+                cv2.destroyAllWindows()
             if self.firstClick:
                 #Convert image to hsv format.
-                hsv = _____________________
+                hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
                 if self.notClickedYet:
                     #Lower bound is determined by taking the h value and subtracting 10
                     #and s and v values are fixed to 50
-                    lower=np.array([_______________,____________,____________])
+                    lower=np.array([self.x+50,50,50])
                     #Upper bound is determined by taking the h value and adding 10
                     #and s and v values are fixed to 255
-                    upper=np.array([____________,_____________,____________])
+                    upper=np.array([self.x+10,255,255])
                     self.notClickedYet=False
                 #Mask of 0 and 1 of the pixels within the range between lower and upper
                 mask = cv2.inRange(hsv, lower, upper)
                 #Apply the mask to the original pic
                 res = cv2.bitwise_and(frame,frame, mask= mask)
                 #Show the mask
-                cv2.imshow('mask',___)
+                cv2.imshow('mask',mask)
                 #Show the tracked color
-                cv2.imshow('res',___)
+                cv2.imshow('res',res)
         cv2.destroyAllWindows()
 
 detect=DetectColor()
